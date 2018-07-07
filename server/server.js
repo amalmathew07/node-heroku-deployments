8709bcab167355c43ff21e63c8c8bf8be93a1c8d
Amal Mathew <>
2018-07-06 23:20:33 -0400

    Commit2

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 3000;
 var app = express();
 
 app.get('/',(req,res) => {
-    res.send("Heroku Dev into Production 2");
+    res.send("Heroku Dev into Production 3");
 });
 
 app.listen(port);
